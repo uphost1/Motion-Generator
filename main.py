baf6874f04f1dc1abc5cd26edba1feac7b97350e
uphost1 <48634234+uphost1@users.noreply.github.com>
2019-03-28 13:52:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,55 @@
-import os
 import datetime
-import base64
-import git
+import os
 from git import Repo
 
-r_dir = 'directory here'
+r_dir = os.path.dirname(os.path.realpath(__file__))
 r = Repo(r_dir)
+github_repo = 'https://github.com/uphost1/test-motions/blob/master/'
 
 today = datetime.date.today().strftime('%d %B, %Y')
 signature = '<p align="right">Signed, your signature</p>'
 
+
 def start_function():
-    op1 = input('[1] Sentencing Order\n[2] Motion to Suppress\n[3] Motion to Compel\n[4] Motion to Dismiss\n[5] Motion for Discovery\n[6] Custom Title\nInput: ')
+    op1 = input('[1] Sentencing Order\n[2] Motion to Suppress\n[3] Motion to Compel\n[4]'
+                ' Motion to Dismiss\n[5] Motion for Discovery\n[6] Custom Title\nInput: ')
+
     def input_stuff(ftype):
         global fileName, header, op4, fileType
         fileType = ftype
         op2 = input('Plaintiff: ')
         op3 = input('Defendant: ')
-        fileName = op2 + '-v-' + op3 + datetime.datetime.now().strftime('%Y-%m-%d@%H-%M') + '.md'
+        case = f'{op2} v. {op3}'
+        fileName = f'{op2}-v-{op3}date{datetime.datetime.now().strftime("%Y-%m-%d@%H-%M")}.md'
         op4 = input('Body Text: ')
-        header = '\n# In the Federal District Court of the United States\n###### ' + op2 + ' v. ' + op3 + '\n' + '<b>' + ftype + '</b>'
+        header = f'\n# In the Federal District Court of the United States\n###### {case}\n<b>{ftype}</b>'
+
     def save_stuff():
-        newVerdict = open(fileName, 'w')
-        newVerdict.write(today + header +'<p>' + op4 + '</p>\n' + signature)
-        newVerdict.close()
-        print(newVerdict.name, 'created')
+        new_verdict = open(fileName, 'w')
+        new_verdict.write(f'{today}{header}<p>{op4}</p>\n{signature}')
+        new_verdict.close()
+        print(new_verdict.name, 'created')
         
         file_list = [
-            'C:\\Users\westo\Desktop\Test\\'+fileName
+            f'{os.path.dirname(os.path.realpath(__file__))}/{fileName}'
             ]
         r.index.add(file_list)
         r.index.commit(fileType)
         r.git.pull('origin', 'master')
-        r.git.push('origin','master')
-        print('github repo here'+fileName)
+        r.git.push('origin', 'master')
+        print(github_repo+fileName)
         start_function()
     if op1 == '1':
-        input_stuff("SENTENCING ORDER")
+        input_stuff('SENTENCING ORDER')
         save_stuff()
     if op1 == '2':
-        input_stuff("MOTION TO SUPPRESS EVIDENCE")
+        input_stuff('MOTION TO SUPPRESS EVIDENCE')
         save_stuff()
     if op1 == '3':
-        input_stuff("MOTION TO COMPEL")
+        input_stuff('MOTION TO COMPEL')
         save_stuff()
     if op1 == '4':
-        input_stuff("MOTION TO DISMISS")
+        input_stuff('MOTION TO DISMISS')
         save_stuff()
     if op1 == '5':
         input_stuff("MOTION FOR DISCOVERY")
@@ -57,4 +61,7 @@ def start_function():
         print('ok')
         start_function()
 
+
 start_function()
+
+
