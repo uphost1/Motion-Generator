4fa96a79209fe98939b911bde138bfb65f0850ec
uphost1 <48634234+uphost1@users.noreply.github.com>
2019-03-28 19:12:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,30 @@ import datetime
 import os
 from git import Repo
 
+
 r_dir = os.path.dirname(os.path.realpath(__file__))
 r = Repo(r_dir)
-github_repo = '' # Link to github repository
+github_repo = '' # link to your github repository
 
 today = datetime.date.today().strftime('%d %B, %Y')
 signature = '<p align="right">Signed, your signature</p>'
 
 
 def start_function():
-    op1 = input('[1] Sentencing Order\n[2] Motion to Suppress\n[3] Motion to Compel\n[4]'
-                ' Motion to Dismiss\n[5] Motion for Discovery\n[6] Custom Title\nInput: ')
-
+    t_o_f = ['Sentencing Order', 'Motion to Suppress', 'Motion to Compel', 'Motion to Dismiss',
+             'Motion for Discovery', 'Custom Title']
+    for v, i in enumerate(t_o_f):
+        print(f'[{v}]{i}\n')
+    op1 = input('\nInput: ')
+    print(f'Starting input for {t_o_f[int(op1)]}')
+    
     def input_stuff(ftype):
         global fileName, header, op4, fileType
         fileType = ftype
         op2 = input('Plaintiff: ')
         op3 = input('Defendant: ')
         case = f'{op2} v. {op3}'
-        fileName = f'{op2}-v-{op3}date{datetime.datetime.now().strftime("%Y-%m-%d@%H-%M")}.md'
+        fileName = f'{op2}-v-{op3}-{datetime.datetime.now().strftime("%Y-%m-%d@%H-%M")}.md'
         op4 = input('Body Text: ')
         header = f'\n# In the Federal District Court of the United States\n###### {case}\n<b>{ftype}</b>'
 
@@ -39,29 +44,11 @@ def start_function():
         r.git.push('origin', 'master')
         print(github_repo+fileName)
         start_function()
-    if op1 == '1':
-        input_stuff('SENTENCING ORDER')
-        save_stuff()
-    if op1 == '2':
-        input_stuff('MOTION TO SUPPRESS EVIDENCE')
-        save_stuff()
-    if op1 == '3':
-        input_stuff('MOTION TO COMPEL')
-        save_stuff()
-    if op1 == '4':
-        input_stuff('MOTION TO DISMISS')
-        save_stuff()
-    if op1 == '5':
-        input_stuff("MOTION FOR DISCOVERY")
-        save_stuff()
-    if op1 == '6':
-        input_stuff(input('Title: '))
-        save_stuff()
-    else:
-        print('ok')
-        start_function()
+
+    for v, i in enumerate(t_o_f):
+        if op1 == str(v):
+            input_stuff(i)
+            save_stuff()
 
 
 start_function()
-
-
